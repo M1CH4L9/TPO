20f3b3672315ded2dd402b76cf2c17d0d1a54efb
Michał <78867292+M1CH4L9@users.noreply.github.com>
2026-04-03 18:12:04 +0200

    Add files via upload

--- TPO2_BM_S33975/TPO2_BM_S33975/src/zad1/IpWhoIsGeoLookup.java
+++ TPO2_BM_S33975/TPO2_BM_S33975/src/zad1/IpWhoIsGeoLookup.java
@@ -6,16 +6,63 @@
 
 package zad1;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.time.ZoneId;
 
 public class IpWhoIsGeoLookup implements GeoLookup {
+    //wbudowany leniwy klient
+    private final HttpClient client = HttpClient.newHttpClient();
+
   @Override
   public GeoInfo lookup(String ip) throws GeoLookupException {
-    return parseGeoInfo("JSON");
+    try {
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create("https://ipwho.is/" + ip))
+          .GET()
+          .build();
+
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      return parseGeoInfo(response.body());
+    } catch (Exception e) {
+      throw new GeoLookupException("strzal po api jebnal", e);
+    }
   }
 
   public GeoInfo parseGeoInfo(String json) throws GeoLookupException {
-    throw new GeoLookupException("TODO");
+    if (json == null || json.isEmpty()) {
+      throw new GeoLookupException("pusty json");
+    }
+
+    //sprawdzamy czy sukces jest true
+    if (!json.contains("\"success\":true") && !json.contains("\"success\": true")) {
+      throw new GeoLookupException("api zwrocilo blad albo success na false");
+    }
+
+    //parsowanie po kosztach bez żadnej biblioteki
+    String countryCode = extractRegex(json, "\"country_code\"\\s*:\\s*\"([^\"]+)\"");
+    String zoneId = extractRegex(json, "\"timezone\"\\s*:\\s*\\{[^}]*\"id\"\\s*:\\s*\"([^\"]+)\"");
+
+    if (countryCode == null || zoneId == null) {
+      throw new GeoLookupException("brak pol w jsonie");
+    }
+
+    try {
+      return new GeoInfo(countryCode, ZoneId.of(zoneId));
+    } catch (Exception e) {
+      // zlapie jak string strefy jest jakis z kosmosu
+      throw new GeoLookupException("nierozpoznana strefa czasowa", e);
+    }
   }
+  //pomocnicze żeby nie pisać za dużo
+  private String extractRegex(String text, String regex) {
+    Matcher m = Pattern.compile(regex).matcher(text);
+    return m.find() ? m.group(1) : null;
+  }
+    
 }
