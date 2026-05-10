fc222e56465d7b869c437d25a891bbd7e9e80ecd
Michał Berlak <78867292+M1CH4L9@users.noreply.github.com>
2026-05-10 03:34:47 +0200

    Add files via upload

--- TPO5_BM_S33975/TPO5_BM_S33975/src/zad1/ChatServer.java
+++ TPO5_BM_S33975/TPO5_BM_S33975/src/zad1/ChatServer.java
@@ -31,29 +31,33 @@ public class ChatServer {
     }
 
     public void startServer() {
-        serverThread = new Thread(() -> {
-            try {
-                serverSocket = new ServerSocket(port);
-                isRunning = true;
-                System.out.println("Server started"); //wymagane przez polecenie
+        try {
+            // BUG FIX :))))
+            //otwieramy port w głównym wątku żeby metoda startServer() nie zakończyła się, dopóki port nie będzie gotowy
+            serverSocket = new ServerSocket(port);
+            isRunning = true;
+            System.out.println("Server started");
 
+            //tutaj wątek czeka na klientów
+            serverThread = new Thread(() -> {
                 while (isRunning) {
                     try {
                         Socket clientSocket = serverSocket.accept();
                         ClientHandler handler = new ClientHandler(clientSocket);
+                        //dodajemy do listy, żeby móc od razu bezpiecznie zamknąć port klienta w stopServer()
+                        clients.add(handler);
                         //obsługa żądań odbywa się w wirtualnych wątkach
                         Thread.startVirtualThread(handler);
                     } catch (IOException e) {
-                        if (isRunning) {
-                            e.printStackTrace();
-                        }
+                        //ignorujemy wyjątek wyrzucany w momencie gdy stopServer() zamyka gniazdo
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        serverThread.start();
+            });
+            serverThread.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void stopServer() {
@@ -65,11 +69,15 @@ public class ChatServer {
             if (serverSocket != null) {
                 serverSocket.close();
             }
+
+            //zatrzymujemy ubicie gniazd TCP na 50ms, żeby "chat closed" na 100% dotarło do klientów
+            Thread.sleep(50);
+
             //zamykamy połączenia wszystkich aktualnie podłączonych klientów
             for (ClientHandler client : clients) {
                 client.close();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -97,6 +105,7 @@ public class ChatServer {
         private PrintWriter out;
         private Scanner in;
         private String clientId;
+        private boolean isLoggedIn = false; //kinda ważne - flaga czy klient zdążył wejść
 
         public ClientHandler(Socket socket) {
             this.socket = socket;
@@ -121,17 +130,15 @@ public class ChatServer {
                     if (message.startsWith("LOGIN:")) {
                         clientId = message.substring(6);
                         String msg = clientId + " logged in";
-                        //dodajemy klienta i natychmiast rozsyłamy jego logowanie
                         synchronized (ChatServer.this) {
-                            clients.add(this);
+                            isLoggedIn = true; //zamiast clients.add(this)
                             broadcastAndLog(msg);
                         }
                     } else if (message.startsWith("LOGOUT:")) {
                         String msg = clientId + " logged out";
-                        //rozsyłamy info i od razu usuwamy klienta z listy
                         synchronized (ChatServer.this) {
                             broadcastAndLog(msg);
-                            clients.remove(this);
+                            isLoggedIn = false; //odznaczamy flagę
                         }
                         break;
                     } else if (message.startsWith("MSG:")) {
@@ -149,7 +156,8 @@ public class ChatServer {
         }
 
         public void sendMessage(String message) {
-            if (out != null) {
+            //warunek - klient musi być zalogowany, żeby otrzymać powiadomienie
+            if (isLoggedIn && out != null) {
                 out.println(message);
             }
         }
